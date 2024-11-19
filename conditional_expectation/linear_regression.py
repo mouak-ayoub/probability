84c63f385c035de4c83fd3a60ce77ef214f18fe1
MOUAK-AYOUB <ayoub.mouak.2015@gmail.com>
2024-11-20 00:16:55 +0100

    Simplify plot parameters

--- conditional_expectation/linear_regression.py
+++ conditional_expectation/linear_regression.py
@@ -3,14 +3,13 @@ from scipy import stats
 import numpy as np
 import matplotlib.pylab as plt
 
-number_samples = 10000
+number_samples = 1000
 b = 5
 a = 3
 normal = stats.norm(0, 1)
 x_samples = normal.rvs(number_samples)
 y_samples = a + b * x_samples + normal.rvs(number_samples)
-x, y = zip(*sorted(zip(x_samples, y_samples)))
-plt.scatter(x, y)
+plt.scatter(x_samples, y_samples)
 plt.show()
 covariance_matrix = np.cov(x_samples, y_samples)
 b_approx = covariance_matrix[0][1] / covariance_matrix[0][0]
--- graphical_summaries/histogram.py
+++ graphical_summaries/histogram.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+
 # Creating distribution
 x = [10.1, 12.2, 9.3, 12.4, 13.7, 11.1, 13.3, 10.8, 11.6, 10.1, 11.2, 11.4, 11.8, 7.1,
      12.2, 12.6, 9.2, 14.2, 10.5]
@@ -20,8 +21,8 @@ axs.hist(x, bins=n_bins, density=True)
 fig.show()
 
 fig2, axs2 = plt.subplots(1, 1,
-                        figsize=(10, 7),
-                        tight_layout=True)
+                          figsize=(10, 7),
+                          tight_layout=True)
 axs2.boxplot(x)
 fig2.show()
 plt.show(
